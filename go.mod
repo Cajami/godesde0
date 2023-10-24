b5154eeabd0521b49b7aab8a5f3b6ad2d021a424
Javier Huiñocana <javier2315@gmail.com>
2023-10-23 20:01:28 -0500

    primer programa y primer paquete

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cajami/godesde0
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/cajami/godesde0/variables"
+
+)
+
+func main() {
+
+	variables.MuestroEnteros()
+
+}
